c5a4a36ea833e23b6be5fe813bedf9abba3c5c39
matteoetzer@gmx.at <matteoetzer@gmx.at>
2020-03-05 08:36:15 +0100

    CSharp Script Testing

---
+++ Assets/MyGame/Scripts/Skript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skript : MonoBehaviour
+{
+    public int Test = 69;
+    public GameObject NamensAenderung;
+    public string Name;
+    public float xPos;
+    void Start()
+    {
+        Debug.Log(Test);
+        Debug.Log(xPos);
+    }
+    void Update()
+    {
+        NamensAenderung.name = Name;
+        xPos = NamensAenderung.transform.position.x;
+    }
+}
